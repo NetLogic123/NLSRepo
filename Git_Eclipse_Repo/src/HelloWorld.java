20f2a78214c19fc0ff8917237ae4b41c050d5cab
keystrat <keystrat@keystrat-PC>
2016-02-10 11:01:31 -0600

    second change

--- Git_Eclipse_Repo/src/HelloWorld.java
+++ Git_Eclipse_Repo/src/HelloWorld.java
@@ -7,6 +7,8 @@ public class HelloWorld {
 		System.out.println("Hello World, This is from Eclipse to test Git");
 		
 		System.out.println("Hello, This is from Git-Thankyou");
+		
+		System.out.println("Great I got the message");
 
 	}
 
